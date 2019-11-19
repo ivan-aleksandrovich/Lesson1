c92bf096bc6a30d4642c60dfee1242ce9ebd7e4d
Ivan <ak_dlia_hc_ru@mail.ru>
2019-11-19 20:59:58 +0300

    2

---
+++ 2.rb
@@ -0,0 +1,13 @@
+=begin
+Площадь треугольника. Площадь треугольника можно вычислить,
+зная его основание (a) и высоту (h) по формуле: 1/2*a*h. 
+Программа должна запрашивать основание и высоту треугольника 
+и возвращать его площадь.
+=end
+puts "Write your base"
+base = gets.chomp.to_i
+puts "Write your height"
+height= gets.chomp.to_i
+result =  0,5*base*height
+
+print 'result = ' , result[1]
